779ce85fee3d1be42ecc356a22b38c33043c0538
Vasil Kolev <v.s.kolev@gmail.com>
2024-11-22 12:38:13 +0200

    modified:   src/app/user-input/user-input.component.ts

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -45,7 +45,7 @@ export class UserInputComponent {
     //////////////////////////////// reset inputs
     this.enteredInitialInvestment.set('0');
     this.enteredAnnualInvestment.set('0');
-    this.enteredExpectedReturn.set('0');
-    this.enteredDuration.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
